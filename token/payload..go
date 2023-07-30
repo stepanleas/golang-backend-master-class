da8f958a58b06fafaf576d284016e38faab7c22f
stepan <s.leas@developmentaid.org>
2023-07-30 14:53:19 +0300

    add jwt and paseto tokens. Write tests for them

---
+++ token/payload..go
@@ -0,0 +1,46 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Different types of error returned by the VerifyToken function
+var (
+	ErrInvalidToken = errors.New("token is invalid")
+	ErrExpiredToken = errors.New("token has expired")
+)
+
+// Payload contains the payloda data of the token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+}
+
+// NewPayload creates a new token payload with a specific username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenId, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenId,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiresAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
+
+// Valid checks if the token payload is valid or not
+func (payload *Payload) Valid() error {
+	if time.Now().After(payload.ExpiresAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
